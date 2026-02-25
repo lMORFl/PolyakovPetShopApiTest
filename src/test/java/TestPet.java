2e88d640cd00976d2c516e28fa4daeca739ad00f
Administrator <morfiklol777@gmail.com>
2026-02-25 17:15:09 +0700

    Добавил отчетность Allure в автотест

--- src/test/java/TestPet.java
+++ src/test/java/TestPet.java
@@ -1,7 +1,12 @@
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,19 +15,26 @@ public class TestPet {
     private static final String BASE_URL = "http://5.181.109.28:9090/api/v3";
 
     @Test
+    @Feature("Pet")
+    @Severity(SeverityLevel.CRITICAL)
+    @Owner("Polyakov Semyon")
     public void testDeleteNonexistentPet() {
-        Response response = given()
+        Response response = step ("Отправляем DELETE запрос на удаление питомца", () ->
+        given()
                 .contentType(ContentType.JSON)
                 .header("Accept", "application/json")
                 .when()
-                .delete(BASE_URL+ "/pet/9999");
+                .delete(BASE_URL+ "/pet/9999"));
 
         String responseBody = response.getBody().asString();
 
-        assertEquals(200, response.getStatusCode(),
-                "Код ответа не совпал с ожидаемым. Ответ: " + responseBody);
+        step("Проверяем статус-код == 200", () ->
+                assertEquals(200, response.getStatusCode(),
+                "Код ответа не совпал с ожидаемым. Ответ: " + responseBody));
 
-        assertEquals("Pet deleted", responseBody,
-                "Текст ответа не совпал с ожидаемым. Получен: " + responseBody);
+
+        step("Проверяем текст ответа == 'Pet deleted'", () ->
+                assertEquals("Pet deleted", responseBody,
+                "Текст ответа не совпал с ожидаемым. Получен: " + responseBody));
     }
 }
